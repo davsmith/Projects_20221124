eb7d81c66ea4bbb680e135d83b257aa1ab6186ba
Dave Smith <davsmith@users.noreply.github.com>
2022-11-22 18:38:52 -0800

    Added QPainter example

--- Python/_Tutorials/_Python/pyqt_intro/course/_15a_painter_rect.py
+++ Python/_Tutorials/_Python/pyqt_intro/course/_15a_painter_rect.py
@@ -1,43 +1,93 @@
+"""Example code from PyQt Course for Beginners tutorial (YouTube).
+
+PyQt6 Course for Beginners - Parwiz Forogh (https://youtu.be/ot94H3-d5d8?t=10388)
+Detailed notes: https://tinyurl.com/y5db3c5c
+
+Demonstrates drawing shapes using the QPainter class
+
+Concepts introduced:
+    - Drawing rectangles
+    - Differences between PyQt5 and PyQt6
+
+Module/Class/Methods:
+    QApplication    See detailed notes (linked above)
+    QWidget         See detailed notes (linked above)
+    QLabel          See detailed notes (linked above)
+    QIcon           See detailed notes (linked above)
+    QFont           See detailed notes (linked above)
+
+    QPainter        Draws shapes in a window (canvas)
+        paintEvent  Callback for 
+    QPainter This show the longest line allowed by Python style conventions (99 characters)
+
+
+Created November 22, 2022
+"""
+
+from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtGui import QIcon, QFont, QPainter, QPen, QBrush
+from PyQt6.QtCore import Qt
+
+from os.path import join, realpath, dirname
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout
-from PyQt6.QtWidgets import QTableWidget,QTableWidgetItem
-from PyQt6.QtGui import QIcon, QFont
 
 class Window(QWidget):
+    """Single line summary of the class with a period at the end.
+ 
+    Methods:
+        __init__(self, arg1, arg2)
+        method2()
+    """
 
     def __init__(self):
-        super().__init__()
-        self.setWindowTitle('PyQt6 TableWidget')
-        self.setWindowIcon(QIcon("app_icon.png"))
-        self.setGeometry(500, 200, 500, 400)
+      """This method multiplies the given two numbers.
+  
+      Input Arguments: a, b must be numbers.
+      Returns: Multiplication of a and b.
+      """
+      super().__init__()
 
-        vbox = QVBoxLayout()
+      script_path = realpath(dirname(__file__))
+      app_icon = QIcon(join(script_path, "images/app_icon.png"))
 
-        # add table 
-        tableWidget = QTableWidget(self)
-        tableWidget.setColumnCount(3)
-        tableWidget.setRowCount(3)
-        tableWidget.setStyleSheet('background-color:yellow')
-        tableWidget.setFont(QFont('Times New Roman', 14))
+      # Set window attributes
+      self.setWindowTitle("PyQt6 Drawing Shapes QPainter()")
+      self.setWindowIcon(app_icon)
+      self.setGeometry(500, 300, 500, 300)
 
-        tableWidget.setItem(0, 0, QTableWidgetItem("FName"))
-        tableWidget.setItem(0, 1, QTableWidgetItem("LName"))
-        tableWidget.setItem(0, 2, QTableWidgetItem("Email"))
+    #   self.create_widgets()
 
-        tableWidget.setItem(1, 0, QTableWidgetItem("Parwiz"))
-        tableWidget.setItem(1, 1, QTableWidgetItem("Forogh"))
-        tableWidget.setItem(1, 2, QTableWidgetItem("parwiz@gmail.com"))
+    def paintEvent(self, e):
+        painter = QPainter(self)
+        painter.setPen(QPen(Qt.GlobalColor.blue, 5, Qt.PenStyle.DashDotDotLine))
 
-        tableWidget.setItem(2, 0, QTableWidgetItem("John"))
-        tableWidget.setItem(2, 1, QTableWidgetItem("Doe"))
-        tableWidget.setItem(2, 2, QTableWidgetItem("john_doe@gmail.com"))
+        painter.drawRect(100, 15, 300, 100)
 
-        vbox.addWidget(tableWidget)
-        self.setLayout(vbox)
 
-        self.show()
+    def create_widgets(self):
+      """This method multiplies the given two numbers.
 
+      Input Arguments: a, b must be numbers.
+      Returns: Multiplication of a and b.
+      """
+
+      # Create a widget for something, set some attributes
+      widget1 = QWidget("Click Me", self)
+
+      # widget1.move(100,100)
+      widget1.setGeometry(100, 100, 100, 100)
+      widget1.setStyleSheet('background-color:red')
+      widget1.setIcon(QIcon('button_icon.png'))
+
+      # Create a widget for something, set some attributes
+      label = QLabel("My Label", self)
+      label.move(100,200)
+      label.setStyleSheet('color:green')
+      label.setFont(QFont("Times New Roman", 15))
+
+# *** Main ***
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    frame = Window()
-    sys.exit(app.exec())
+  app = QApplication([])
+  window = Window()
+  window.show()
+  sys.exit(app.exec())
